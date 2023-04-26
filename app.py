9d72b0f8c28bbf60b54be4d98853872f8e01eea9
Janani Rajadurai <jananirajadurai@Jananis-MacBook-Pro.local>
2023-04-26 16:55:45 -0500

    Added csv file and re-titled pages and buttons

--- app.py
+++ app.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template()
+    return render_template('index.html')
+
+@app.route('/points')
+def vmd_timestamp():
+    return render_template('points_page.html')
